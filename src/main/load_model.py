94b40750c963cb10b4ab37656590d0d47237d484
andonokar <anderson_igmy@hotmail.com>
2024-03-25 15:10:28 -0300

    load model function

---
+++ src/main/load_model.py
@@ -0,0 +1,10 @@
+import joblib
+
+
+def load_model():
+    """
+    Function that loads the model trained in pkl located in resources folder
+    :return: The trained Linear Regression model
+    """
+    linear_regression = joblib.load(f"main/resources/linear_regression_model.pkl")
+    return linear_regression
