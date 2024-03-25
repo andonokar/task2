f967d3311c4565b8545a9e40c69a8c4795ce24ae
andonokar <anderson_igmy@hotmail.com>
2024-03-25 15:09:49 -0300

    main app

---
+++ src/main/__main__.py
@@ -0,0 +1,13 @@
+from load_data import load_data, COLUMNS_CHECK
+from load_model import load_model
+from aggregate_result import aggregate_result
+import json
+
+if __name__ == "__main__":
+    PATH = "main/predictions/predictions.json"
+    data = load_data()
+    prediction = load_model().predict(data[COLUMNS_CHECK])
+    final_df = aggregate_result(data, prediction)
+    final_df.to_json(PATH, orient="records")
+    with open(PATH, "r") as file:
+        print(json.load(file))
